49ce1df1ca6aef8a16e37d51690812749b00cb98
yashnw <yashwalavalkar0077@gmail.com>
2024-05-26 13:17:36 +0530

    commit successfully

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      {/* This is a navbar */}
+      <div className="d-flex align-items-center justify-content-between shadow-sm ">
+        <img
+          src="https://almablog-media.s3.ap-south-1.amazonaws.com/01_Alma_Better_Logo_caa4b55aad.png"
+          alt="Alma Better Logo"
+          style={{ height: "70px", width: "190px" }}
+        />
+        {/* Here for going to other componenet used a react routing */}
+        <div className="d-flex">
+          <Link to="/template" className="nav-link m-2">
+            Resume Template
+          </Link>
+          <Link to="/myresume" className="nav-link m-2">
+            My Resume
+          </Link>
+          <Link to="/about" className="nav-link m-2">
+            About Us
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
